9a47fc35391b1019781e1fbcac2f830721ca89a7
amrares <rares03avram@gmail.com>
2024-05-17 22:02:56 +0300

    gata

--- src/app/product/[productName]/page.tsx
+++ src/app/product/[productName]/page.tsx
@@ -41,7 +41,8 @@ function Page(props: any) {
     }, [name]); // Run this effect whenever the product name changes
 
     const handleColorSelect = (color: any) => {
-        setSelectedColor(() => {{
+        setSelectedColor(() => {
+            {
                 const selectedColor = selectedProduct.colors.find((c: any) => c.color === color);
                 if (selectedColor) {
                     setProductImage(selectedColor.imageUrl);
@@ -50,8 +51,8 @@ function Page(props: any) {
             }
         });
     };
-    
-      
+
+
     const handleAddToCart = () => {
         if (selectedSize && selectedColor) {
             const newItem = {
@@ -64,6 +65,7 @@ function Page(props: any) {
             // Clear selection after adding to cart
             setSelectedSize(null);
             setSelectedColor("black");
+            window.location.reload();
         } else {
             alert("Please select both size and color.");
         }
@@ -78,46 +80,43 @@ function Page(props: any) {
                     <h1 className="text-l text-white brightness-75 text uppercase">Oversized fit</h1>
 
                     <h1 className="text-l mt-10 text-white brightness-75 text uppercase">size guide</h1>
-                    
+
                     {/* Size selection */}
                     <div className="flex mt-12 gap-10">
-                    {["S", "M", "L", "XL", "XXL"].map((size) => (
-                        <button
-                        key={size}
-                        className={`px-2 py-1 transition-all duration-300 hover:brightness-100 hover:scale-110 border-green-400 border-[2px] rounded-xl ${
-                            selectedSize === size
-                            ? "brightness-100 text-white scale-110"
-                            : "brightness-75 text-white border-transparent"
-                        }`}
-                        onClick={() => handleSizeSelect(size)}
-                        >
-                        {size}
-                        </button>
-                    ))}
+                        {["S", "M", "L", "XL", "XXL"].map((size) => (
+                            <button
+                                key={size}
+                                className={`px-2 py-1 transition-all duration-300 hover:brightness-100 hover:scale-110 border-green-400 border-[2px] rounded-xl ${selectedSize === size
+                                        ? "brightness-100 text-white scale-110"
+                                        : "brightness-75 text-white border-transparent"
+                                    }`}
+                                onClick={() => handleSizeSelect(size)}
+                            >
+                                {size}
+                            </button>
+                        ))}
                     </div>
 
                     {/* Color selection */}
                     <div className="flex mt-12 text-white font-bold text-l gap-10">
                         {[
-                        { name: "spatial black", color: "black" },
-                        { name: "sunlight white", color: "white" },
+                            { name: "spatial black", color: "black" },
+                            { name: "sunlight white", color: "white" },
                         ].map(({ name, color }) => (
-                        <button
-                            key={color}
-                            className={`p-2 uppercase flex gap-4 hover:scale-110 transition-all duration-300 border-green-400 border-[2px] rounded-xl ${
-                            selectedColor === color
-                                ? "brightness-100 scale-110"
-                                : "brightness-75 border-transparent"
-                            }`}
-                            onClick={() => handleColorSelect(color)}
-                        >
-                            <div
-                            className={`w-6 h-6 rounded-full ${
-                                color === "black" ? "bg-black" : "bg-white"
-                            }`}
-                            ></div>
-                            <p className="transition-all duration-300">{name}</p>
-                        </button>
+                            <button
+                                key={color}
+                                className={`p-2 uppercase flex gap-4 hover:scale-110 transition-all duration-300 border-green-400 border-[2px] rounded-xl ${selectedColor === color
+                                        ? "brightness-100 scale-110"
+                                        : "brightness-75 border-transparent"
+                                    }`}
+                                onClick={() => handleColorSelect(color)}
+                            >
+                                <div
+                                    className={`w-6 h-6 rounded-full ${color === "black" ? "bg-black" : "bg-white"
+                                        }`}
+                                ></div>
+                                <p className="transition-all duration-300">{name}</p>
+                            </button>
                         ))}
                     </div>
 
